85ef5d591c6adbd676d3f3ee51afc81eb6acf900
NEMO2345 <phamthily2222002@gmail.com>
2023-08-27 18:58:15 +0700

    create rider_app

--- flutter_myapp/android/app/src/main/java/com/phamthily/fluttermyapp/flutter_myapp/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.phamthily.fluttermyapp.flutter_myapp;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ rider_app/android/app/src/main/kotlin/com/phamthily/rider_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.phamthily.rider_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

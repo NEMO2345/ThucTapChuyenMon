756c10ca773ca58098f902a3564c1f6071f6825a
NEMO2345 <phamthily2222002@gmail.com>
2023-09-01 21:22:57 +0700

    add home and work location

--- rider_app/lib/AllScreens/mainscreen.dart
+++ rider_app/lib/AllScreens/mainscreen.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:rider_app/AllWidgets/Divider.dart';
 
 class MainScreen extends StatefulWidget {
   static const String idScreen = "mainScreen";
@@ -10,11 +12,115 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  get initialCameraPosition => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Main Screen"),
+      ),
+      body: Stack(
+        children: [
+        /*  GoogleMap(initialCameraPosition: initialCameraPosition),*/
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: Container(
+              height: 320.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15.0),
+                  topRight: Radius.circular((18.0)),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 16.0,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.7, 0.7),
+                  )
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0,vertical: 18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 6.0),
+                    Text("Hi There, ",style: TextStyle(fontSize: 12.0),),
+                    Text("Where to? ",style: TextStyle(fontSize: 20.0,fontFamily: "Brand-Bold"),),
+                    SizedBox(height:20.0 ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(
+                         5.0
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: 6.0,
+                            spreadRadius: 0.5,
+                            offset: Offset(0.7, 0.7),
+                          )
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          children: [
+                            Icon(Icons.search,color: Colors.blueAccent,),
+                            SizedBox(width: 10.0,),
+                            Text("Search Drop off"),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 24.0,),
+                    Row(
+                      children: [
+                        Icon(Icons.home,color: Colors.grey,),
+                        SizedBox(width: 12.0,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Add Home"),
+                            SizedBox(height: 4.0,),
+                            Text("Your living home address",style: TextStyle(color: Colors.grey[200],fontSize:12.0 ),),
+
+                          ],
+                        ),
+                      ],
+                    ),
+
+                    SizedBox(height: 10.0,),
+                    DividerWidget(),
+                    SizedBox(height: 16.0,),
+
+                    Row(
+                      children: [
+                        Icon(Icons.work,color: Colors.grey,),
+                        SizedBox(width: 12.0,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Add Work"),
+                            SizedBox(height: 4.0,),
+                            Text("Your office address",style: TextStyle(color: Colors.grey[200],fontSize:12.0 ),),
+
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

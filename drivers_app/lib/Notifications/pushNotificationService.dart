07a4cedee4f78c4ce74312500ace2761c87eda22
NEMO2345 <phamthily2222002@gmail.com>
2023-09-21 23:25:57 +0700

    send notification using httpRequest data message

--- drivers_app/lib/Notifications/pushNotificationService.dart
+++ drivers_app/lib/Notifications/pushNotificationService.dart
@@ -1,44 +1,68 @@
 // ignore_for_file: avoid_print
-
 import 'package:drivers_app/configMaps.dart';
 import 'package:drivers_app/main.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 
 class PushNotificationService {
   final FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
 
-  Future initialize() async {
+  Future<void> initialize() async {
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       print("onMessage: $message");
     });
+
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       print("onMessageOpenedApp: $message");
     });
+
     FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
     // Handle onLaunch
     RemoteMessage? initialMessage = await firebaseMessaging.getInitialMessage();
     if (initialMessage != null) {
       print("onLaunch: $initialMessage");
     }
+
     // Handle onResume
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print("onResume: $message");
+      // print("onResume: $message");
+      getRideRequest(message.data);
     });
-  }
-  Future<dynamic> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-    print("onBackgroundMessage: $message");
-  }
-  Future<String> getToken() async {
-    String? token = await firebaseMessaging.getToken();
-    print("this is token :: ");
-    print(token);
 
-    driversRef.child(currentfirebaseUser!.uid).child("token").set(token);
+    await firebaseMessaging.requestPermission();
+    String? token = await getToken();
+    if (token != null) {
+      driversRef.child(currentfirebaseUser!.uid).child("token").set(token);
+    }
 
     firebaseMessaging.subscribeToTopic("alldrivers");
     firebaseMessaging.subscribeToTopic("allusers");
+  }
 
-    return token!;
+  Future<String?> getToken() async {
+    String? token = await firebaseMessaging.getToken();
+    print("this is token :: ");
+    print(token);
+    return token;
+  }
+
+  String getRideRequest(Map<String, dynamic> message) {
+    String rideRequestId = "";
+    if (Platform.isAndroid) {
+      print("this is Ride Request Id :: ");
+      rideRequestId = message['data']['ride_request_id'];
+      print(rideRequestId);
+    } else {
+      print("this is Ride Request Id :: ");
+      rideRequestId = message['ride_request_id'];
+      print(rideRequestId);
+    }
+    return rideRequestId;
+  }
+
+  Future<void> _firebaseMessagingBackgroundHandler(
+      RemoteMessage message) async {
+    print("onBackgroundMessage: $message");
   }
 }

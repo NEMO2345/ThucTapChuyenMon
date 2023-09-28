9f54b43db91f93badd18683980f365ea01da5901
NEMO2345 <phamthily2222002@gmail.com>
2023-09-28 21:57:27 +0700

    display new ride request dialog to driver (accept and cancel new ride request)

--- drivers_app/lib/main.dart
+++ drivers_app/lib/main.dart
@@ -28,24 +28,24 @@ Future<void> setupFlutterNotifications() async {
   if (isFlutterLocalNotificationsInitialized) {
     return;
   }
-  channel = const AndroidNotificationChannel(
-    'high_importance_channel', // id
-    'High Importance Notifications', // title
-    description:
-    'This channel is used for important notifications.', // description
-    importance: Importance.high,
-  );
+  // channel = const AndroidNotificationChannel(
+  //   'high_importance_channel', // id
+  //   'High Importance Notifications', // title
+  //   description:
+  //   'This channel is used for important notifications.', // description
+  //   importance: Importance.high,
+  // );
 
-  var flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  //var flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
   /// Create an Android Notification Channel.
   ///
   /// We use this channel in the `AndroidManifest.xml` file to override the
   /// default FCM channel to enable heads up notifications.
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-      AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
+  // await flutterLocalNotificationsPlugin
+  //     .resolvePlatformSpecificImplementation<
+  //     AndroidFlutterLocalNotificationsPlugin>()
+  //     ?.createNotificationChannel(channel);
 
   /// Update the iOS foreground notification presentation options to allow
   /// heads up notifications.
